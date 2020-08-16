fd16757c6bf7a1aa637dc38c4da4f044efe867b1
netwarp <resethread@gmail.com>
2020-08-16 16:18:26 +0200

    add migrations and update config database

--- app/config/database.php
+++ app/config/database.php
@@ -14,7 +14,7 @@ use Spiral\Database\Driver;
 return [
     'default'   => 'default',
     'databases' => [
-        'default' => ['driver' => 'runtime'],
+        'default' => ['driver' => 'mysql'],
     ],
     'drivers'   => [
         'runtime' => [
@@ -22,5 +22,15 @@ return [
             'connection' => 'sqlite:' . directory('root') . 'app.db',
             'profiling'  => true,
         ],
+        'mysql'     => [
+            'driver'     => Driver\MySQL\MySQLDriver::class,
+            'options'    => [
+                'connection' => 'mysql:host=127.0.0.1;dbname=' . env('DB_NAME'),
+                'username'   => env('DB_USERNAME'),
+                'password'   => env('DB_PASSWORD'),
+            ]
+        ],
     ]
 ];
+
+
---
+++ app/migrations/20200816.125139_0_my_migration.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * {project-name}
+ * 
+ * @author {author-name}
+ */
+declare(strict_types=1);
+
+namespace App;
+
+use Spiral\Migrations\Migration;
+
+class MyMigrationMigration extends Migration
+{
+    /**
+     * Create tables, add columns or insert data here
+     */
+    public function up(): void
+    {
+        $this->table('users')
+            ->addColumn('id', 'primary')
+            ->addColumn('name', 'string')
+            ->addColumn('email', 'string')
+            ->addColumn('password', 'string')
+            ->create();
+    }
+
+    /**
+     * Drop created, columns and etc here
+     */
+    public function down(): void
+    {
+        $this->table('users')->drop();
+    }
+}
